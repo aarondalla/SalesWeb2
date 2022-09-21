03c4c0c0d91f8097345fb544ca3da0469542618b
aarondalla <aaron.dalla@gmail.com>
2022-09-21 05:27:51 -0300

    Project created

---
+++ SalesWeb2022/Models/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWeb2022.Models.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
